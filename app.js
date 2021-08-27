a40a3f686762c3d00c898adbe4d6b9da4bde8677
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-08-28 00:26:27 +0600

    Update Resposiblity

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const searchFood = () => {
         const searchField = document.getElementById('search-field');
         const searchText = searchField.value;
         searchField.value = '';
+        if (searchText.length == 0) {
+                alert('Please Write Something')
+        }
+        else {
 
-        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-        fetch(url)
-                .then(res => res.json())
-                .then(data => displayFood(data.meals))
+                const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+                fetch(url)
+                        .then(res => res.json())
+                        .then(data => displayFood(data.meals))
+        }
 }
 
 //Display Search Food
@@ -52,14 +57,14 @@ const displayMealDetails = (meal) => {
         mealDetail.innerText = '';
         const div = document.createElement('div');
         div.innerHTML = `
-                        <div class= "card mx-auto w-25">
+                        <div style="width:350px" class= "card mx-auto">
                         <img src="${meal.strMealThumb}" class="card-img-top p-4" alt="...">
                                 <div class="card-body">
                                         <h5 class="card-title">${meal.strMeal}</h5>
                                         <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
                                         <a target="_blank" href="${meal.strYoutube}" class="btn btn-danger">How to Cook</a>
                                 </div>
-        </div>
+                        </div>
         `
         mealDetail.appendChild(div);
 
